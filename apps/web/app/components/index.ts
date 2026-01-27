8c833efbf9827e6438e2242aa986775b7ad91c78
bingoYB <binhdu@foxmail.com>
2026-01-27 16:14:51 +0800

    style(web): 清理组件导出和格式
    移除未使用的 DownloadSection 导出，统一引号格式，更新 Next.js 类型路径

--- apps/web/app/components/index.ts
+++ apps/web/app/components/index.ts
@@ -1,5 +1,4 @@
-export { Header } from './Header';
-export { Footer } from './Footer';
-export { FeatureShowcase } from './FeatureShowcase';
-export { DownloadSection } from './DownloadSection';
-export * from './demos';
+export { Header } from "./Header";
+export { Footer } from "./Footer";
+export { FeatureShowcase } from "./FeatureShowcase";
+export * from "./demos";
